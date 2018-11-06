e8c4d0579b76cd59d4c5d7f288b8cc968974cd22
rossmarino <10914084+RossMar@users.noreply.github.com>
2018-11-06 12:18:51 -0500

    move all R scripts to same folder

---
+++ data/MA/r_scripts/demo_results.R
@@ -0,0 +1,49 @@
+library(tidyverse)
+library(readxl)
+demo_name<-c("asian", "black", "disabled", "econ_dis",
+             "ell", "female", "hispanic", "high_needs", "male", 
+             "multiracial", "non_econ_dis", "white")
+
+#### ELA Passing Percentage  --  by Demographic ####
+
+setwd('~/Desktop/DS-5110-Group-Project/data/MA/PARCC/by_demo/ela/')
+demo_list<-map(list.files(path = '.', pattern = "*"), ~ read_excel(., skip = 7))
+
+names(demo_list)<-demo_name
+
+test<-map(demo_list, ~ summarise(.,
+                              tot_pass = sum(`L4+5 #`, na.rm=T),
+                              tot_inc = sum(`Stud. Incl. #`, na.rm =T),
+                              pass_per = tot_pass/tot_inc))
+for(i in 1:length(test)){
+  test[[i]]<-test[[i]]%>%mutate(demo = demo_name[i])
+}
+sum_demo<-as.tibble(test[[1]])
+for(i in 2:length(test)){
+  sum_demo<-union(sum_demo, as.tibble(test[[i]]))
+}
+
+sum_demo%>%mutate(demo = reorder(demo, pass_per))%>%ggplot(aes(x=demo, y=pass_per))+
+  geom_col()+coord_flip()+labs(y="Percent of Students Passing", title="ELA Passing Percentage by Demographic", x="")
+
+#### MATH Passing Percentage  --  by Demographic####
+
+
+setwd('~/Desktop/DS-5110-Group-Project/data/MA/PARCC/by_demo/math/')
+demo_math<-map(list.files(path = '.', pattern = "*"), ~ read_excel(., skip = 7))
+
+test2<-map(demo_math, ~ summarise(.,
+                                 tot_pass = sum(`L4+5 #`, na.rm=T),
+                                 tot_inc = sum(`Stud. Incl. #`, na.rm =T),
+                                 pass_per = tot_pass/tot_inc))
+for(i in 1:length(test2)){
+  test2[[i]]<-test2[[i]]%>%mutate(demo = demo_name[i])
+}
+sum_math<-as.tibble(test2[[1]])
+for(i in 2:length(test2)){
+  sum_math<-union(sum_math, as.tibble(test2[[i]]))
+}
+
+sum_math%>%mutate(demo = reorder(demo, pass_per))%>%ggplot(aes(x=demo, y=pass_per))+
+  geom_col()+coord_flip()+labs(x='', y="Percent of Students Passing", title="Math Passing Percent by Demographics")
+
---
+++ data/MA/r_scripts/ma_prelim.R
@@ -0,0 +1,46 @@
+
+## MA PARCC DATA ##
+#grades 3-8 together
+parcc_ma_ela<-read_excel('./data/MA/PARCC/ELA.xls', skip = 7)
+parcc_ma_math<-read_excel('./data/MA/PARCC/MATH.xls', skip = 7)
+parcc_ma<-union(parcc_ma_ela, parcc_ma_math)
+
+ma<-list()
+
+#use list.files() function in future
+ma$ela3<-read_excel('./data/MA/PARCC/3ELA.xls', skip = 7, na = 'N/A')
+ma$math3<-read_excel('./data/MA/PARCC/3MATH.xls', skip = 7,na = 'N/A')
+ma$ela4<-read_excel('./data/MA/PARCC/4ELA.xls', skip = 7, na = 'N/A')
+ma$math4<-read_excel('./data/MA/PARCC/4MATH.xls', skip = 7, na = 'N/A')
+ma$ela5<-read_excel('./data/MA/PARCC/5ELA.xls', skip = 7, na = 'N/A')
+ma$math5<-read_excel('./data/MA/PARCC/5MATH.xls', skip = 7, na = 'N/A')
+ma$ela6<-read_excel('./data/MA/PARCC/6ELA.xls', skip = 7, na = 'N/A')
+ma$math6<-read_excel('./data/MA/PARCC/6MATH.xls', skip = 7, na = 'N/A')
+ma$ela7<-read_excel('./data/MA/PARCC/7ELA.xls', skip = 7, na = 'N/A')
+ma$math7<-read_excel('./data/MA/PARCC/7MATH.xls', skip = 7, na = 'N/A')
+ma$ela8<-read_excel('./data/MA/PARCC/8ELA.xls', skip = 7, na = 'N/A')
+ma$math8<-read_excel('./data/MA/PARCC/8MATH.xls', skip = 7, na = 'N/A')
+ma$alg8<-read_excel('./data/MA/PARCC/8ALG.xls', skip = 7, na = 'N/A')
+
+ma<-map(ma, ~ filter(.,!is.na(`Grade/Subject`))%>%
+          separate(`Grade/Subject`, c('Grade', "Subject"), sep = 7)%>%
+          mutate(`SGP #` = as.numeric(`SGP #`),`Trans. SGP Median` = as.numeric(`Trans. SGP Median`)))
+
+
+ma_tib<-ma[[1]]
+for(i in 2:length(ma)){
+  ma_tib<-union(ma_tib, ma[[i]])
+}
+ma_tib<-arrange(ma_tib, `Org Name`, Grade)
+
+ma_math<-ma_tib%>%group_by(Grade)%>%filter(Subject == ' Math')%>%
+  summarise(avg_math = mean(`Average Scaled Score`))
+ma_ela<-ma_tib%>%group_by(Grade)%>%filter(Subject == ' ELA/L')%>%
+  summarise(avg_ela = mean(`Average Scaled Score`))
+ggplot(full_join(ma_math, ma_ela, by = 'Grade'), aes(x=avg_ela))+
+  geom_point(aes(y=avg_math, color = Grade))+
+  labs(title="                      MA avg. PARCC scores", x="ELA", y="MATH")
+
+ggplot(full_join(ma_math, ma_ela, by = 'Grade'))+
+  geom_point(aes(x=Grade, y=avg_math, color="Math"))+geom_point(aes(x=Grade, y=avg_ela, color='ELA'))+
+  labs(title="Scaled score by Grade", y="Scaled Score")
